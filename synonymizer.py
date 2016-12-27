959582e1a6e363baf45da25edb0dc48167dff98a
Mouse Reeve <mousereeve@riseup.net>
2016-12-27 12:00:06 -0800

    Wordnik api key in settings

--- synonymizer.py
+++ synonymizer.py
@@ -1,8 +1,9 @@
 ''' reword bot '''
-
 import re
 import settings
 from TwitterAPI import TwitterAPI
+from wordnik.swagger import ApiClient
+from wordnik.WordApi import WordApi
 
 
 sentence = 'I love to talk about nothing. ' \
@@ -15,8 +16,15 @@ api = TwitterAPI(settings.TWITTER_API_KEY, settings.API_SECRET,
 tokens = sentence.split(' ')
 
 # find synonyms
+client = ApiClient(settings.WORDNIK_API_KEY, 'http://api.wordnik.com/v4')
+api = WordApi(client)
+
 for word in tokens:
-    word = re.sub(r'[\.,?!]', '', word)
+    word = re.sub(r'[\.,i\?!]', '', word)
+
+    results = api.getRelatedWord(word, useCanonical=True,
+                                 relationshipTypes='synonym')
+    import pdb;pdb.set_trace()
 
 
 # restructure text
