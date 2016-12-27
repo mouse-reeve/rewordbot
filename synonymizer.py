8c1f72da0574a994179d01f6b0f5b8636c3431eb
Mouse Reeve <mousereeve@riseup.net>
2016-12-27 12:11:57 -0800

    Working synonymizer

--- synonymizer.py
+++ synonymizer.py
@@ -1,4 +1,5 @@
 ''' reword bot '''
+import random
 import re
 import settings
 from TwitterAPI import TwitterAPI
@@ -9,24 +10,31 @@ from wordnik.WordApi import WordApi
 sentence = 'I love to talk about nothing. ' \
            'It\'s the only thing I know anything about.'
 
-api = TwitterAPI(settings.TWITTER_API_KEY, settings.API_SECRET,
-                 settings.TWITTER_ACCESS_TOKEN, settings.ACCESS_SECRET)
+twitter_api = TwitterAPI(settings.TWITTER_API_KEY,
+                         settings.TWITTER_API_SECRET,
+                         settings.TWITTER_ACCESS_TOKEN,
+                         settings.TWITTER_ACCESS_SECRET)
 
 # tokenize
 tokens = sentence.split(' ')
 
 # find synonyms
 client = ApiClient(settings.WORDNIK_API_KEY, 'http://api.wordnik.com/v4')
-api = WordApi(client)
+wordnik_api = WordApi(client)
 
+reworded = []
 for word in tokens:
-    word = re.sub(r'[\.,i\?!]', '', word)
+    word = re.sub(r'[\.,\?!]', '', word)
 
-    results = api.getRelatedWord(word, useCanonical=True,
-                                 relationshipTypes='synonym')
-    import pdb;pdb.set_trace()
-
+    results = wordnik_api.getRelatedWords(word, useCanonical=True,
+                                          relationshipTypes='synonym')
+    try:
+        options = results[0].words
+        reworded.append(random.choice(options))
+    except TypeError:
+        reworded.append(word)
 
 # restructure text
+print ' '.join(reworded)
 
 # tweet
