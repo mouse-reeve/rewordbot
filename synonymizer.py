af34565da913f8277453e7cbb26b6a79806e4d41
Mouse Reeve <mousereeve@riseup.net>
2016-12-27 08:58:17 -0800

    Some basic structures

--- synonymizer.py
+++ synonymizer.py
@@ -1,8 +1,23 @@
-# load tweets
+''' reword bot '''
+
+import re
+import settings
+from TwitterAPI import TwitterAPI
+
+
+sentence = 'I love to talk about nothing. ' \
+           'It\'s the only thing I know anything about.'
+
+api = TwitterAPI(settings.TWITTER_API_KEY, settings.API_SECRET,
+                 settings.TWITTER_ACCESS_TOKEN, settings.ACCESS_SECRET)
 
 # tokenize
+tokens = sentence.split(' ')
 
 # find synonyms
+for word in tokens:
+    word = re.sub(r'[\.,?!]', '', word)
+
 
 # restructure text
 
